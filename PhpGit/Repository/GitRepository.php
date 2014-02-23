a0b0074412983b2abd550f0996eda81ef9ec45cf
kouinkouin <me@kouinkouin.com>
2014-02-23 18:57:51 +0100

    First Delivery - Start Repository class

---
+++ PhpGit/Repository/GitRepository.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace PhpGit\Repository;
+
+/**
+ * Class Repository\GitRepository
+ * @author  Raphaël Pommier (raphael@pommier.me)
+ * @since   2014-02-22
+ */
+class GitRepository {
+
+	private $remote = '';
+
+	public function __construct($remote = 'origin') {
+		$this->remote = (string) $remote;
+	}
+
+	/**
+	 * @param string $gitCommand
+	 * @return string
+	 */
+	private function getGitQuery($gitCommand) {
+		return escapeshellcmd('git '.(string) $gitCommand);
+	}
+
+	private function getPathsString(array $paths) {
+		array_walk($paths, function (&$path) {
+			$path = "'$path'";
+		});
+		return implode(' ', $paths);
+	}
+
+	public function getCommit($message, $options = '') {
+		return $this->getGitQuery('commit '.($options ? $options.' ' : '').'-m "'.$message.'"');
+	}
+
+	public function getStatus() {
+		return $this->getGitQuery('status --porcelain');
+	}
+
+	public function getPush($branch = 'HEAD', $withTags = true) {
+		return $this->getGitQuery('push --porcelain '.($withTags ? '--tags ' : '').$this->remote.' '.$branch);
+	}
+
+	public function getFetch() {
+		return $this->getGitQuery('fetch -v '.$this->remote);
+	}
+
+	public function getAdd(array $files) {
+		return $this->getGitQuery('add -v '.$this->getPathsString($files));
+	}
+
+	public function getRm(array $files, $isRecursive = true, $fromStaged = false) {
+		return $this->getGitQuery('rm -q '.($isRecursive ? '-r ' : '').($fromStaged ? '--cached ' : '').$this->getPathsString($files));
+	}
+
+	public function getCheckout($branch, $create = false) {
+		return $this->getGitQuery('checkout -q '.($create ? '--no-track -b ' : '').$branch);
+	}
+
+
+}
+ 
---
+++ tests/Repository/GitRepositoryTest.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace PhpGitTests\Repository;
+
+use PhpGit\Repository\GitRepository;
+
+require_once __DIR__.'/../../PhpGit/Repository/GitRepository.php';
+
+/**
+ * Class GitRepositoryTest
+ * @author  Raphaël Pommier (raphael@pommier.me)
+ * @since   2014-02-22
+ */
+class GitRepositoryTest extends \PHPUnit_Framework_TestCase {
+
+	public function testGetCommit() {
+		$gitRepository = new GitRepository();
+		$assertCommand = "git commit -m \"It\\'s a new test !\"";
+		$this->assertSame($assertCommand, $gitRepository->getCommit("It's a new test !"));
+	}
+
+	public function testGetCommitWithOptions() {
+		$gitRepository = new GitRepository();
+		$assertCommand = "git commit --amend -m \"It\\'s a new test !\"";
+		$this->assertSame($assertCommand, $gitRepository->getCommit("It's a new test !", '--amend'));
+	}
+
+	public function testGetStatus() {
+		$gitRepository = new GitRepository();
+		$assertCommand = "git status --porcelain";
+		$this->assertSame($assertCommand, $gitRepository->getStatus());
+	}
+
+	public function testGetPush() {
+		$gitRepository = new GitRepository();
+		$assertCommand = "git push --porcelain --tags origin HEAD";
+		$this->assertSame($assertCommand, $gitRepository->getPush());
+	}
+
+	public function testGetPushWithoutTags() {
+		$gitRepository = new GitRepository();
+		$assertCommand = "git push --porcelain origin HEAD";
+		$this->assertSame($assertCommand, $gitRepository->getPush('HEAD', false));
+	}
+
+	public function testGetFetch() {
+		$gitRepository = new GitRepository();
+		$assertCommand = "git fetch -v origin";
+		$this->assertSame($assertCommand, $gitRepository->getFetch());
+	}
+
+	public function testGetAdd() {
+		$gitRepository = new GitRepository();
+		$assertCommand = "git add -v 'azerty.txt' 'qsd fgh.lst'";
+		$this->assertSame($assertCommand, $gitRepository->getAdd(array('azerty.txt', 'qsd fgh.lst')));
+	}
+
+	public function testGetRmNoRecursive() {
+		$gitRepository = new GitRepository();
+		$assertCommand = "git rm -q 'azerty.txt'";
+		$this->assertSame($assertCommand, $gitRepository->getRm(array('azerty.txt'), false));
+	}
+
+	public function testGetRmCached() {
+		$gitRepository = new GitRepository();
+		$assertCommand = "git rm -q -r --cached 'azerty.d' 'azerty.txt'";
+		$this->assertSame($assertCommand, $gitRepository->getRm(array('azerty.d', 'azerty.txt'), true, true));
+	}
+
+	public function testGetCheckout() {
+		$gitRepository = new GitRepository();
+		$assertCommand = "git checkout -q mybranch";
+		$this->assertSame($assertCommand, $gitRepository->getCheckout('mybranch'));
+	}
+
+	public function testGetCheckoutBranch() {
+		$gitRepository = new GitRepository();
+		$assertCommand = "git checkout -q --no-track -b mybranch";
+		$this->assertSame($assertCommand, $gitRepository->getCheckout('mybranch', true));
+	}
+
+	/*
+			public function testGetMerge() {
+				$gitRepository = new GitRepository();
+				$assertCommand = "git merge --no-ff mybranch";
+				$this->assertSame($assertCommand, $gitRepository->getMerge('mybranch'));
+			}
+
+			public function testGetMergeWithMessage() {
+				$gitRepository = new GitRepository();
+				$assertCommand = "git merge --no-ff mybranch";
+				$this->assertSame($assertCommand, $gitRepository->getMerge('mybranch', 'Merge happy branch into master'));
+			}
+
+			public function testGetRebase() {
+				$gitRepository = new GitRepository();
+				$assertCommand = "git rebase master";
+				$this->assertSame($assertCommand, $gitRepository->getRebase('master'));
+			}
+
+			public function testGetRebaseOnto() {
+				$gitRepository = new GitRepository();
+				$assertCommand = "git rebase --onto master oldmaster";
+				$this->assertSame($assertCommand, $gitRepository->getRebase('master', 'oldmaster'));
+			}
+
+			/*
+			 * git clean
+			 * git reset --hard
+			 * git reset --mixed
+			 * git reset --soft
+			 * git clone
+			 * git tag -a 0.1 -m "Stable release 0.1"
+			 *
+			 */
+}
